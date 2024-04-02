a539f2526518dd4400559db9a22211f262a4d17d
Abdellatif <abdelt.2012@gmail.com>
2024-04-02 03:08:11 +0000

    Done.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,8 +4,6 @@ Create Flask App.
 """
 
 from flask import Blueprint
+from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import specific views from api.v1.views
-from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Create Flask App
 from flask import jsonify
 from api.v1.views import app_views  # Import the blueprint
 
+
 @app_views.route('/status')
 def api_status():
     """Returns a JSON response indicating the API status."""
