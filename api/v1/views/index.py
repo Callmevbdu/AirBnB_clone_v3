1cded9a8902af636b4a21f6954ebd8a689d1e837
Abdellatif <abdelt.2012@gmail.com>
2024-04-02 04:16:21 +0000

    Done.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,10 +9,31 @@ from models import storage
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
-def api_status():
+def get_status():
     """Returns a JSON response indicating the API status."""
     data = {
         "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("api/v1/stats", methods=['GET'], strict_slashes=False)
+def get_stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
     }
 
     resp = jsonify(data)
