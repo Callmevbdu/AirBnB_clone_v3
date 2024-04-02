8b334f4bc64c743c32e4b06a9725c8759dd1648f
Abdellatif <abdelt.2012@gmail.com>
2024-04-02 03:53:07 +0000

    Done.

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,17 +3,43 @@
 Create Flask App.
 """
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
+from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 # Register blueprint
 app.register_blueprint(app_views)
 
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    teardown function
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    response = jsonify(data)
+    response.status_code = 404
+
+    return (response)
+
+
 # Run the Flask server
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,18 @@ Create Flask App
 """
 
 from flask import jsonify
-from api.v1.views import app_views  # Import the blueprint
+from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status')
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def api_status():
     """Returns a JSON response indicating the API status."""
-    return jsonify({'status': 'OK'})
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
