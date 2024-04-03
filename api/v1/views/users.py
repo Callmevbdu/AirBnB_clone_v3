95f52489c810c3f89a88bf6d8f5778a754bacba2
Abdellatif <abdelt.2012@gmail.com>
2024-04-03 02:42:47 +0000

    Done.

---
+++ api/v1/views/users.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+"""
+defines a set of Flask routes for managing User objects within an API.
+"""
+from flask import jsonify, abort, request
+from api.v1.views import app_views, storage
+from models.user import User
+
+
+@app_views.route("/users", methods=["GET"], strict_slashes=False)
+def user_get_all():
+    """
+    Retrieves all User objects and returns them as a JSON list.
+    """
+    user_list = []
+    user_obj = storage.all("User")
+    for obj in user_obj.values():
+        user_list.append(obj.to_json())
+
+    return jsonify(user_list)
+
+
+@app_views.route("/users", methods=["POST"], strict_slashes=False)
+def user_create():
+    """
+    Creates a new User object from provided JSON data (including email and
+    password) and returns the newly created user with a 201 Created status code
+    """
+    user_json = request.get_json(silent=True)
+    if user_json is None:
+        abort(400, 'Not a JSON')
+    if "email" not in user_json:
+        abort(400, 'Missing email')
+    if "password" not in user_json:
+        abort(400, 'Missing password')
+
+    new_user = User(**user_json)
+    new_user.save()
+    resp = jsonify(new_user.to_json())
+    resp.status_code = 201
+
+    return resp
+
+
+@app_views.route("/users/<user_id>",  methods=["GET"], strict_slashes=False)
+def user_by_id(user_id):
+    """
+    Gets a specific User object by its ID and returns it as JSON. Returns a 404
+    Not Found status code if the user is not found.
+    """
+
+    fetched_obj = storage.get("User", str(user_id))
+
+    if fetched_obj is None:
+        abort(404)
+
+    return jsonify(fetched_obj.to_json())
+
+
+@app_views.route("/users/<user_id>",  methods=["PUT"], strict_slashes=False)
+def user_put(user_id):
+    """
+    Updates a specific User object with provided JSON data, excluding certain
+    fields like ID, email, and timestamps. Returns the updated user object with
+    a 200 OK status code or a 400 Bad Request or 404 Not Found status code on
+    failure.
+    """
+    user_json = request.get_json(silent=True)
+
+    if user_json is None:
+        abort(400, 'Not a JSON')
+
+    fetched_obj = storage.get("User", str(user_id))
+
+    if fetched_obj is None:
+        abort(404)
+
+    for key, val in user_json.items():
+        if key not in ["id", "created_at", "updated_at", "email"]:
+            setattr(fetched_obj, key, val)
+
+    fetched_obj.save()
+
+    return jsonify(fetched_obj.to_json())
+
+
+@app_views.route("/users/<user_id>",  methods=["DELETE"], strict_slashes=False)
+def user_delete_by_id(user_id):
+    """
+    Deletes a specific User object by its ID and returns an empty JSON
+    dictionary with a 200 OK status code or a 404 Not Found status code if the
+    user is not found.
+    """
+
+    fetched_obj = storage.get("User", str(user_id))
+
+    if fetched_obj is None:
+        abort(404)
+
+    storage.delete(fetched_obj)
+    storage.save()
+
+    return jsonify({})
